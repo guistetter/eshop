1963836e93116806871aee7ee64e8a8f2835e3e5
guistetter <guilherme_stetter@hotmail.com>
2020-07-25 16:28:53 -0300

    renderizando categorias de produtos na home

--- bancoDados/scriptsBanco.sql
+++ bancoDados/scriptsBanco.sql
@@ -145,3 +145,5 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 INSERT INTO `devShop`.`categories` (`id`, `category`, `description`) VALUES ('1', 'Eletronicos', 'Eletronicos');
+INSERT INTO `devShop`.`categories` (`id`, `category`, `description`) VALUES ('2', 'Eletrodomesticos', 'Eletrodomesticos');
+INSERT INTO `devShop`.`categories` (`id`, `category`, `description`) VALUES ('3', 'Celulares', 'Celulares');
