10a12a78469e01061ab34281df5a25ce54a443b2
guistetter <guilherme_stetter@hotmail.com>
2020-07-25 15:19:34 -0300

    configurando epxress para arquivos estativos

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
 app.get("/", (req,res) => {
 res.send("DevShop")
 })
