8b60c53a5b7eb2013a9985c573538e532839645a
guistetter <guilherme_stetter@hotmail.com>
2020-07-25 17:44:46 -0300

    filtrando os produs pelas categorias na pagina home, ao clicar em eletronicos no menu de produtos, ir para pagina produtos listando apenas os eletronicos

--- index.js
+++ index.js
@@ -22,8 +22,19 @@ app.use(express.static("public"))
 app.get("/", async (req,res) => {
   const categories = await db("categories").select("*")
   res.render("home",{
-    categories:categories
+    categories
   })
+})
+
+app.get("/categoria/:id", async(req, res) =>{
+  const products = await db("products").select("*").where("id", function(){
+    this
+    .select("categories_products.product_id")
+    .from("categories_products")
+    .whereRaw("categories_products.product_id = products.id")
+    .where("categorie_id", req.params.id)
+  })
+  res.send(products)
 })
 
 app.listen(port, err =>{
