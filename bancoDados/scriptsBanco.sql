6f0e52db572ed2c5dcab747a9f175dfa044600d8
guistetter <guilherme_stetter@hotmail.com>
2020-07-25 16:21:47 -0300

    iniciando e testando conexao com banco de dados

--- bancoDados/scriptsBanco.sql
+++ bancoDados/scriptsBanco.sql
@@ -144,3 +144,4 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+INSERT INTO `devShop`.`categories` (`id`, `category`, `description`) VALUES ('1', 'Eletronicos', 'Eletronicos');
--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
+const db = require("knex")({
+  client: "mysql2",
+  connection: {
+    host: '127.0.0.1',
+    user: "root",
+    password: "123456",
+    database: "devShop"
+  }
+})
+
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
-app.get("/", (req,res) => {
-res.render("home")
+app.get("/", async (req,res) => {
+  const categories = await db("categories").select("*")
+res.render("home",{
+  categories:categories
+})
 })
 
 app.listen(port, err =>{
