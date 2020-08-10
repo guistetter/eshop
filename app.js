7651c680e12f33cb4cf56d5285e66ea3403e1e9a
guistetter <guilherme_stetter@hotmail.com>
2020-08-10 18:07:14 -0300

    passando user para o view

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 
-const init = db => {const category = require("./models/category")
-const routes = require("./routes")
+const init = db => {
+  const category = require("./models/category")
+  const routes = require("./routes")
 
-const express = require("express")
-const app = express()
+  const express = require("express")
+  const app = express()
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json({extended: true}))
-app.use(bodyParser.urlencoded())
+  const bodyParser = require('body-parser')
+  const session = require('express-session')
 
-app.set("view engine", "ejs")
-app.use(express.static("public"))
+  app.use(bodyParser.json({extended: true}))
+  app.use(bodyParser.urlencoded())
+  app.use(session({
+    secret: 'MyDevShopRulez!',
+    name: "sessionId"
+  }))
 
-//middleware para adicionar categoria ao menu
-app.use(async (req, res, next) => {
-  const categories = await category.getCategories(db)()
-    res.locals = {
-      categories
-    }
-    next()
-  })
-  app.use(routes(db))
-  return app
+  app.set("view engine", "ejs")
+  app.use(express.static("public"))
+
+  //middleware para adicionar categoria ao menu
+  app.use(async (req, res, next) => {
+    const categories = await category.getCategories(db)()
+    const {user} = req.session
+      res.locals = {
+        categories,
+        user
+      }
+      next()
+    })
+    app.use(routes(db))
+    return app
 }
 module.exports = init
