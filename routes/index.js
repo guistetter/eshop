f59d408a1121c27b96c27c69c5c0629eaedd1963
guistetter <guilherme_stetter@hotmail.com>
2020-08-10 17:05:31 -0300

    criando rota post para fazer login

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const init = db => {
+  const auth = require("../controllers/auth")
+
   const home = require("../controllers/home")
 
   const categories = require("./categories")
   const products = require("./products")
 
   const router = require("express").Router()
+
+  router.post('/login',auth.login(db))
   router.get("/", home.getIndex)
   router.use("/categoria", categories(db))
   router.use("/produto", products(db))
