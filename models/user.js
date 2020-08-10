c3fb93da1a72af9a11c565d8526a76707c6634c5
guistetter <guilherme_stetter@hotmail.com>
2020-08-10 16:26:47 -0300

    criando usuario base

--- models/user.js
+++ models/user.js
@@ -1,13 +1,22 @@
+const bcrypt = require('bcryptjs')
+
+const generatePassHash = passwd =>{
+   const salt = bcrypt.genSaltSync(10)
+   const hash = bcrypt.hashSync(passwd, salt)
+   return hash
+}
+
 const initialUser= db => async(id) => {
   const count = await db("users").count('id as total')
   if(count[0].total === 0){
     const user = {
       name: 'Admin',
       email: "admin@devshop.com.br",
-      passwd: 'a definir',
+      passwd: generatePassHash('MinhaSenhaDificil!'),
       email_checked: true,
       created: new Date(),
-      updated: new Date()
+      updated: new Date(),
+      roles: 'admin,financial,customer'
     }
     await db('users').insert(user)
   }
