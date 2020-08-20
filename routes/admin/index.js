4c77b21efcf55375fa5c0569a30015121e0a9f9e
guistetter <guilherme_stetter@hotmail.com>
2020-08-20 13:29:01 -0300

    criando midleware para validar se o usuario é admin, se nao for nao pode acessar administrativo e volta pra home

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -4,8 +4,21 @@ const init = db => {
   const categories = require("./categories")
   //const products = require("./products")
 
+  //autorizacao para admin baseado em papel
+  router.use((req,res,next) => {
+    if(req.session.user){
+      if(req.session.user.roles.indexOf('admin') < 0){
+        res.redirect('/')
+      } else {
+        next()
+      }
+    } else{
+      res.redirect('/login')
+    }
+  })
+  
   router.get('/',(req,res) => res.send('admin'))
-  router.use("/categoria", categories(db))
+  router.use("/categorias", categories(db))
   //router.use("/produto", products(db))
   return router
 }
