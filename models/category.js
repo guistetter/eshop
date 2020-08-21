1737fd27732acadc2ba32fcb1fadae60325684f9
guistetter <guilherme_stetter@hotmail.com>
2020-08-21 15:13:18 -0300

    criando funcao init'wrapper' injetando o banco de dados, removendo db dos metodos, chamando na importacao

--- models/category.js
+++ models/category.js
@@ -1,50 +1,57 @@
-const slug = require("../utils/slug")
-const Joi = require('@hapi/joi')
-const validation = require("../utils/validation")
-//validar dados
-const createSchema = Joi.object().keys({
-  category: Joi.string().min(5).max(245).required(),
-  description: Joi.string().min(5).required()
-})
+const init = db => {
 
-const getCategoriesById = db => async(id) => {
-  const category = await db("categories")
-    .select("*")
-    .where('id', id)
-  return category
-}
+  const slug = require("../utils/slug")
+  const Joi = require('@hapi/joi')
+  const validation = require("../utils/validation")
 
-const getCategories = db => async () =>{
-  const categories = await db("categories").select("*")
-  const categoriasWithSlug = categories.map( category => {
-    const newCategory = {...category, slug: slug(category.category)}
-    return newCategory
+  //validar dados
+  const createSchema = Joi.object().keys({
+    category: Joi.string().min(5).max(245).required(),
+    description: Joi.string().min(5).required()
   })
-  return categoriasWithSlug
+
+  const getCategoriesById = async(id) => {
+    const category = await db("categories")
+      .select("*")
+      .where('id', id)
+    return category
+  }
+
+  const getCategories = async() =>{
+    const categories = await db("categories").select("*")
+    const categoriasWithSlug = categories.map( category => {
+      const newCategory = {...category, slug: slug(category.category)}
+      return newCategory
+    })
+    return categoriasWithSlug
+  }
+
+  const createCategory = async(category) => {
+    //se nao conseguir inserir a exception é passada pro controller e tratamos lá
+      const value = validation.validate(category, createSchema)
+      await db('categories').insert(value)
+      return true
+  }
+
+  const removeCategory = async(id) =>{
+    await db('categories').where({id}).del()
+  }
+
+  const updateCategory = async(id, category) => {
+    //se nao conseguir inserir a exception é passada pro controller e tratamos lá
+      const value = validation.validate(category, createSchema)
+      await db('categories').where({id}).update(value)
+      return true
+  }
+
+  return{
+    getCategories, 
+    getCategoriesById, 
+    createCategory,
+    removeCategory,
+    updateCategory
+  }
+  
 }
 
-const createCategory = db => async(category) => {
-  //se nao conseguir inserir a exception é passada pro controller e tratamos lá
-    const value = validation.validate(category, createSchema)
-    await db('categories').insert(value)
-    return true
-}
-
-const removeCategory = db => async(id) =>{
-  await db('categories').where({id}).del()
-}
-
-const updateCategory = db => async(id, category) => {
-  //se nao conseguir inserir a exception é passada pro controller e tratamos lá
-    const value = validation.validate(category, createSchema)
-    await db('categories').where({id}).update(value)
-    return true
-}
-
-module.exports = {
-  getCategories, 
-  getCategoriesById, 
-  createCategory,
-  removeCategory,
-  updateCategory
-}
+module.exports = init
