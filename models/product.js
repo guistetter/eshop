c546ae1f3479b8d78c140fd11b7642d1f48f042f
guistetter <guilherme_stetter@hotmail.com>
2020-08-24 12:39:22 -0300

    pegando o req.query qye vem do controller categories e criando funcao getPaginationParams

--- models/product.js
+++ models/product.js
@@ -1,12 +1,24 @@
 const init = db => {
+  
+  const getPaginationParams = query => {
+    const {currentPage, pages, pageSize} = query
+    return {
+      currentPage: currentPage ? parseInt(currentPage) : 0,
+      pages: pages ? parseInt(pages) : 1,
+      pageSize: pageSize ? parseInt(pageSize) : 10
+    }
+  }
 
-  const getProductsByCategoryId = async (id) => {
+  const getProductsByCategoryId = async (id, query) => {
+    const pagination = getPaginationParams(query)
+    console.log(pagination)
+    //console.log(currentPage, pages, pageSize)
     const products = await db("products").select("*").whereIn("id", function(){
       this
       .select("categories_products.product_id")
       .from("categories_products")
       .where("categorie_id", id)
-    })
+    }).offset(pagination.currentPage * pagination.pageSize).limit(pagination.pageSize)
     return products
   }
 
