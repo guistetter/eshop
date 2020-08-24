a341561beee23579b4225cb68d4cd4fc0779f969
guistetter <guilherme_stetter@hotmail.com>
2020-08-24 12:14:50 -0300

    refatorando query em getProductsByCategoryId trocando where por whereIn

--- models/product.js
+++ models/product.js
@@ -1,16 +1,25 @@
 const init = db => {
 
   const getProductsByCategoryId = async (id) => {
-    const products = await db("products").select("*").where("id", function(){
+    const products = await db("products").select("*").whereIn("id", function(){
       this
       .select("categories_products.product_id")
       .from("categories_products")
-      .whereRaw("categories_products.product_id = products.id")
       .where("categorie_id", id)
     })
     return products
   }
 
+  // const getProductsByCategoryId = async (id) => {
+  //   const products = await db("products").select("*").where("id", function(){
+  //     this
+  //     .select("categories_products.product_id")
+  //     .from("categories_products")
+  //     .whereRaw("categories_products.product_id = products.id")
+  //     .where("categorie_id", id)
+  //   })
+  //   return products
+  // }
   const getProductById = async(id) => {
     const product = await db("products").select("*").where("id",id)
     return product[0]
